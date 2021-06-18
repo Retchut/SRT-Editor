e23b4309f66045bf5bf69ee7c6d1b36dba4fce9e
Mário Travassos <retchut@protonmail.ch>
2021-06-18 01:49:28 +0100

    feat: implement string spllitter

---
+++ src/utils.cpp
@@ -0,0 +1,30 @@
+#include "utils.h"
+
+#include <iostream>
+#include <sstream>
+
+//  Kindly appropriated from
+//  https://stackoverflow.com/a/7621814
+std::vector<std::string> splitString(std::string toSplit, std::string delimiters){
+    std::stringstream ss(toSplit);
+    std::string line;
+    std::vector<std::string> words;
+    while(std::getline(ss, line)) 
+    {
+        size_t prev = 0, pos;
+        while ((pos = line.find_first_of(delimiters, prev)) != std::string::npos)
+        {
+            if (pos > prev)
+                words.push_back(line.substr(prev, pos-prev));
+            prev = pos+1;
+        }
+        if (prev < line.length())
+            words.push_back(line.substr(prev, std::string::npos));
+    }
+
+    for(auto s:words){
+        std::cout << s << std::endl;
+    }
+
+    return words;
+}
---
+++ src/utils.h
@@ -0,0 +1,9 @@
+#ifndef SRT_EDITOR_SRC_UTILS_H
+#define SRT_EDITOR_SRC_UTILS_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> splitString(std::string toSplit, std::string delimiters);
+
+#endif  /*  SRT_EDITOR_SRC_UTILS_H  */
